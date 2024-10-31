140683a73e2b78329c314a59de178ebff2152c11
jose-montenegro-romero <149119278+jose-montenegro-romero@users.noreply.github.com>
2024-10-31 14:18:36 -0500

    update port and comment mongo package.

--- src/main/java/com/example/SpringHexagonal/posts/application/find/PostFindUseCase.java
+++ src/main/java/com/example/SpringHexagonal/posts/application/find/PostFindUseCase.java
@@ -1,26 +1,27 @@
-package com.example.SpringHexagonal.posts.application.find;
-
-import com.example.SpringHexagonal.posts.domain.model.PostQuery;
-import com.example.SpringHexagonal.posts.domain.repository.PostQueryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
-
-@Service
-@RequiredArgsConstructor
-public class PostFindUseCase {
-
-    private final PostQueryRepository postQueryRepository;
-
-    public List<PostQuery> findAllPosts(){
-        return this.postQueryRepository.findAllPosts();
-    }
-    public PostQuery findById(Integer id){
-        return this.postQueryRepository.findById(id).orElseThrow();
-    }
-    public List<PostQuery> findAllPostsByUserId(Integer userId){
-        return this.postQueryRepository.searchBy(Map.of("userId",String.valueOf(userId)));
-    }
-}
+//package com.example.SpringHexagonal.posts.application.find;
+//
+//import com.example.SpringHexagonal.posts.domain.model.PostQuery;
+//import com.example.SpringHexagonal.posts.domain.repository.PostQueryRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import reactor.core.publisher.Flux;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class PostFindUseCase {
+//
+//    private final PostQueryRepository postQueryRepository;
+//
+//    public Flux<List<PostQuery>> findAllPosts(){
+//        return this.postQueryRepository.findAllPosts();
+//    }
+//    public Flux<PostQuery> findById(Integer id){
+//        return this.postQueryRepository.findById(id);
+//    }
+//    public Flux<List<PostQuery>> findAllPostsByUserId(Integer userId){
+//        return this.postQueryRepository.searchBy(Map.of("userId",String.valueOf(userId)));
+//    }
+//}
--- src/main/java/com/example/SpringHexagonal/posts/infrastructure/PostQueryRepositoryImpl.java
+++ src/main/java/com/example/SpringHexagonal/posts/infrastructure/PostQueryRepositoryImpl.java
@@ -1,42 +1,42 @@
-package com.example.SpringHexagonal.posts.infrastructure;
-
-import com.example.SpringHexagonal.posts.domain.model.PostQuery;
-import com.example.SpringHexagonal.posts.domain.repository.PostQueryRepository;
-import com.example.SpringHexagonal.posts.infrastructure.outbound.external.JsonPlaceholderAPIClient;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-@Repository
-@RequiredArgsConstructor
-public class PostQueryRepositoryImpl implements PostQueryRepository {
-    private final JsonPlaceholderAPIClient jsonPlaceholderAPIClient;
-    @Override
-    public Flux<PostQuery> findById(int id) {
-        return jsonPlaceholderAPIClient.findPostById(id);
-    }
-
-    @Override
-    public Flux<List<PostQuery>> searchBy(Map<String, String> params) {
-        return null;
-    }
-
-    @Override
-    public Flux<List<PostQuery>> findAllPosts() {
-        return null;
-    }
-
+//package com.example.SpringHexagonal.posts.infrastructure;
+//
+//import com.example.SpringHexagonal.posts.domain.model.PostQuery;
+//import com.example.SpringHexagonal.posts.domain.repository.PostQueryRepository;
+//import com.example.SpringHexagonal.posts.infrastructure.outbound.external.JsonPlaceholderAPIClient;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Repository;
+//import reactor.core.publisher.Flux;
+//
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Optional;
+//
+//@Repository
+//@RequiredArgsConstructor
+//public class PostQueryRepositoryImpl implements PostQueryRepository {
+//    private final JsonPlaceholderAPIClient jsonPlaceholderAPIClient;
 //    @Override
-//    public Flux<?> searchBy(Map<String, String> params) {
-//        return jsonPlaceholderAPIClient.searchByParam(params);
+//    public Flux<PostQuery> findById(int id) {
+//        return jsonPlaceholderAPIClient.findPostById(id);
 //    }
 //
 //    @Override
-//    public Flux<?> findAllPosts() {
-//        return jsonPlaceholderAPIClient.getAllPosts();
+//    public Flux<List<PostQuery>> searchBy(Map<String, String> params) {
+//        return null;
 //    }
-}
+//
+//    @Override
+//    public Flux<List<PostQuery>> findAllPosts() {
+//        return null;
+//    }
+//
+////    @Override
+////    public Flux<?> searchBy(Map<String, String> params) {
+////        return jsonPlaceholderAPIClient.searchByParam(params);
+////    }
+////
+////    @Override
+////    public Flux<?> findAllPosts() {
+////        return jsonPlaceholderAPIClient.getAllPosts();
+////    }
+//}
